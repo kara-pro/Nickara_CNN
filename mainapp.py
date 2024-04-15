d8213ec1ae62f57ae9235ec956fecf87e5ede9f1
kara-pro <44593706+kara-pro@users.noreply.github.com>
2024-04-15 17:55:07 -0400

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import mlflow.pyfunc
+import numpy as np
 
 # Load the MLflow model as a PyFunc model
 model = mlflow.pyfunc.load_model('./337761508546523430/06c7529135bb4305bff712e6f9794078/artifacts/keras_model')
@@ -9,5 +10,5 @@ app = FastAPI()
 @app.post('/predict')
 def predict(data: dict):
     # Assuming data is a dictionary with model input
-    prediction = model.predict(data['data'])
-    return {"prediction": prediction}
+    prediction = model.predict(np.expand_dims(np.asarray(data['data']), axis=0))
+    return {"prediction": str(np.argmax(prediction))}
